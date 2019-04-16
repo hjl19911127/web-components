2e88606f103379bc46a54d960e59f79e05ef2f25
hjl4347570 <hjl4347570@gmail.com>
2019-04-16 14:45:48 +0800

    add new

--- src/components/image-gallery/index.module.js
+++ src/components/image-gallery/index.module.js
@@ -1,15 +1,23 @@
 class ImageGallery extends HTMLElement {
     constructor() {
         super();
-        this.shadowDom = this.attachShadow({mode: 'open'});
-        this.shadowDom.innerHTML = `
-            <style>${ImageGallery.styles}</style>
-            <div>
-                <slot></slot>
-            </div>
-            <div>
-                <slot name="button"></slot>
-            </div>
+        this.shadowDom = this.attachShadow({ mode: 'open' });
+        this.shadowDom.innerHTML = `${ImageGallery.template}`;
+        const template = this.shadowDom.querySelector('#template')
+        this.shadowDom.appendChild(template.content.cloneNode(true));
+    }
+
+    static get template() {
+        return `
+            <template id="template">
+                <style>${ImageGallery.styles}</style>
+                <div>
+                    <slot></slot>
+                </div>
+                <div>
+                    <slot name="button"></slot>
+                </div>    
+            </template>
         `;
     }
 
@@ -30,7 +38,6 @@ class ImageGallery extends HTMLElement {
             }
         `;
     }
-
 }
 
 window.customElements.define('image-gallery', ImageGallery);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 var disabledLogin = false;
 var customLoginComponent;
 
+function createShadowDom() {
+    var element = document.createElement('div');
+    var shadow = element.attachShadow({ mode: 'closed' });
+    shadow.innerHTML = '<div>in shadow</div>';
+    document.body.appendChild(element);
+    console.log(shadow.querySelector('div'))
+}
+
+function createShadowDom() {
+    var element = document.createElement('div');
+    var shadow = element.attachShadow({ mode: 'closed' });
+    shadow.innerHTML = '<div>in shadow</div>';
+    document.body.appendChild(element);
+    console.log(shadow.querySelector('div'))
+}
 function bindEvent() {
     // 自定义事件
     customLoginComponent.addEventListener('login', function(e) {
@@ -19,6 +34,7 @@ function bindEvent() {
 }
 
 window.addEventListener('DOMContentLoaded', function() {
+    createShadowDom();
     // 通过js初始化 组件
     const el = customElements.get('custom-login');
     const myElement = new el();
